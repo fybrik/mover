b5fcf0a8e5959d293d11c59f29788a08265db201
Florian Froese <ffr@zurich.ibm.com>
2021-02-22 19:06:35 +0100

    Improve store_spark_base_cache.sh
    Signed-off-by: Florian Froese <ffr@zurich.ibm.com>

--- ci/store_spark_base_cache.sh
+++ ci/store_spark_base_cache.sh
@@ -7,11 +7,13 @@ echo "Storing spark-base:$SPARK_VERSION"
 newHash=$(docker inspect spark-base:$SPARK_VERSION --format {{.Id}})
 if [[ -f "docker_images/spark-$SPARK_VERSION.tar" ]]; then
   if [[ $(< docker_images/spark-$SPARK_VERSION.hash) != "$newHash" ]]; then
+    mkdir docker_images || true
     docker save -o docker_images/spark-$SPARK_VERSION.tar spark-base:$SPARK_VERSION
   else
     echo "Image already cached"
   fi
 else
+  mkdir docker_images || true
   docker save -o docker_images/spark-$SPARK_VERSION.tar spark-base:$SPARK_VERSION
 fi
 
