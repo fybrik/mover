1b0bd68c534016cfb0891e6850db810ce8d888bf
Florian Froese <ffr@zurich.ibm.com>
2021-02-22 19:12:46 +0100

    Add more debug information
    Signed-off-by: Florian Froese <ffr@zurich.ibm.com>

--- ci/load_spark_base_cache.sh
+++ ci/load_spark_base_cache.sh
@@ -3,7 +3,7 @@
 # SPDX-License-Identifier: Apache-2.0
 set -x
 
-echo "Loading spark-base:$SPARK_VERSION"
+echo "Loading spark-base:$SPARK_VERSION..."
 
 mkdir docker_images || docker load -i docker_images/spark-$SPARK_VERSION.tar || true
 docker inspect spark-base:$SPARK_VERSION --format {{.Id}} > docker_images/spark-$SPARK_VERSION.hash || true
--- ci/store_spark_base_cache.sh
+++ ci/store_spark_base_cache.sh
@@ -2,18 +2,20 @@
 # Copyright 2020 IBM Corp.
 # SPDX-License-Identifier: Apache-2.0
 
-echo "Storing spark-base:$SPARK_VERSION"
+echo "Storing spark-base:$SPARK_VERSION..."
 
 newHash=$(docker inspect spark-base:$SPARK_VERSION --format {{.Id}})
 if [[ -f "docker_images/spark-$SPARK_VERSION.tar" ]]; then
   if [[ $(< docker_images/spark-$SPARK_VERSION.hash) != "$newHash" ]]; then
     mkdir docker_images || true
     docker save -o docker_images/spark-$SPARK_VERSION.tar spark-base:$SPARK_VERSION
+    echo "Image saved!"
   else
-    echo "Image already cached"
+    echo "Image already cached!"
   fi
 else
   mkdir docker_images || true
   docker save -o docker_images/spark-$SPARK_VERSION.tar spark-base:$SPARK_VERSION
+  echo "Image saved!"
 fi
 
